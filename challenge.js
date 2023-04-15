2b22cbb8cc6877ca7a9ff5da61b1dad77a6a3b14
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-04-15 21:07:13 +0430

    initail solution

--- challenge.js
+++ challenge.js
@@ -1,10 +1,17 @@
-module.exports = function sqrt(number) {
-  return sqrt_recursive(number, 0, number)
+ function sqrt(number) {
+  return sqrt_recursive(number, 0)
 }
 
-function sqrt_recursive(number, min_interval, max_interval) {
-  // write your code here
-}
 
-//console.log(sqrt(25))
-//console.log(sqrt(7056))
+  function sqrt_recursive(k, n) {
+    // write your code her
+    if (k === 0 ) {
+      return n;
+    } else {
+      return sqrt_recursive(k - (2 * n + 1), n + 1);
+    }
+  }
+
+  module.exports =sqrt;
+console.log(sqrt(25))
+console.log(sqrt(7056))
